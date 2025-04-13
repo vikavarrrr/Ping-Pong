31da22850d86c5a83d9d27ff35af2b5efef0225b
vikavarrrr <vika63725var@gmail.com>
2025-04-13 13:06:47 +0300

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,39 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+        def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+            super().__init__()
+            self.image = transform.scale(image.load(player_image), (size_x, size_y))
+            self.speed = player_speed
+            self.rect = self.image.get_rect()
+            self.rect.x = player_x
+            self.rect.y = player_y
+        def reset(self):
+            window.blit(self.image, (self.rect.x, self.rect.y))
+
+win_width = 600
+win_height = 500
+
+window = display.set_mode((win_width, win_height))
+display.set_caption('Ping Pong')
+background = transform.scale(image.load('background.jpg'), (win_width, win_height))
+
+clock = time.Clock()
+FPS = 60
+game = True
+finish = False
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.blit(background, (0,0))
+
+    display.update()
+    clock.tick(FPS)
+
+
+
+
