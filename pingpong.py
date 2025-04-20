9600ac07db14e6b0cbac7718f1453a88bb3d97ec
vikavarrrr <vika63725var@gmail.com>
2025-04-20 12:47:30 +0300

    Add files via upload
    Added class Player and two sprites

--- pingpong.py
+++ pingpong.py
@@ -11,12 +11,32 @@ class GameSprite(sprite.Sprite):
         def reset(self):
             window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 135:
+            self.rect.y += self.speed
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 135:
+            self.rect.y += self.speed
+
 win_width = 600
 win_height = 500
+player_img = 'player.png'
+ball_img = 'ball.png'
 
 window = display.set_mode((win_width, win_height))
 display.set_caption('Ping Pong')
 background = transform.scale(image.load('background.jpg'), (win_width, win_height))
+
+player1 = Player(player_img, 5, 5, 25, 130, 5)
+player2 = Player(player_img, 570, 365, 25, 130, 5)
+ball = GameSprite(ball_img, 200, 200, 50, 50, 4)
 
 clock = time.Clock()
 FPS = 60
@@ -30,6 +50,11 @@ while game:
 
     if finish != True:
         window.blit(background, (0,0))
+        player1.update_r()
+        player2.update_1()
+
+        player1.reset()
+        player2.reset()
 
     display.update()
     clock.tick(FPS)
